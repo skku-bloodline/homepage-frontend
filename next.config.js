830e2efafe489952980e1bf8d22297b00b51e553
Jin Hyoung Joo <iamjinhyoungjoo@gmail.com>
2023-03-06 15:38:22 +0900

    Minor fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "user-images.githubusercontent.com",
+      },
+    ],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
