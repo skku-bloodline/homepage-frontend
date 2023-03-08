9830a1c3928a5b10ba0a939a5cb98f2c61a4a931
Jin Hyoung Joo <iamjinhyoungjoo@gmail.com>
2023-03-08 16:03:31 +0900

    Created apply page

---
+++ src/pages/apply/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/shared/Header";
+import Form from "@/components/apply/Form";
+import Footer from "@/components/shared/Footer";
+
+export default function Apply() {
+  return (
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
+  );
+}
